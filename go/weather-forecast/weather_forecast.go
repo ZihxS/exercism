294892520f26644674c9d1c23cd82d3340841e30
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2024-08-18 22:48:36 +0700

    go: weather forecast

---
+++ go/weather-forecast/weather_forecast.go
@@ -0,0 +1,13 @@
+// Package weather provides tools to weather forecasting.
+package weather
+
+// CurrentCondition represents a current condition weather forecast.
+var CurrentCondition string
+
+// CurrentLocation represents a current location weather forecast.
+var CurrentLocation string
+
+// Forecast returns an string weathe condition/forecasting information.
+func Forecast(city, condition string) string {
+	return CurrentLocation + " - current weather condition: " + CurrentCondition
+}
